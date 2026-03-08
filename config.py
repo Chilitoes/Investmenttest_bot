c2569af464f3f70920a355187756292246114f39
Chilitoes <45633233+Chilitoes@users.noreply.github.com>
2026-03-08 19:11:58 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "8623329849:AAGZCSMSQglNJrfwHtu_uinLEmmkSC-EtYE")
-TELEGRAM_CHAT_ID   = os.environ.get("TELEGRAM_CHAT_ID", "418905687")
+TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "")
+TELEGRAM_CHAT_ID   = os.environ.get("TELEGRAM_CHAT_ID", "")
 REPORT_TIME        = os.environ.get("REPORT_TIME", "23:00")
 ALPHA_VANTAGE_KEY  = os.environ.get("ALPHA_VANTAGE_KEY", "")
