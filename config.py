82228d2b1cc8bc1a79c8fd32b034c5857ab6c081
Chilitoes <45633233+Chilitoes@users.noreply.github.com>
2026-03-08 19:05:00 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "")
+TELEGRAM_CHAT_ID   = os.environ.get("TELEGRAM_CHAT_ID", "")
+REPORT_TIME        = os.environ.get("REPORT_TIME", "23:00")
+ALPHA_VANTAGE_KEY  = os.environ.get("ALPHA_VANTAGE_KEY", "")
